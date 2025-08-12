3c559c23da87554cfcf2719ff1d64e3e9b7af32d
Harry de Boer <hldeboer@gmail.com>
2025-08-12 18:42:06 +0200

    no knmi obs test

--- tests/unit/test_dataplatform_knmi_api_request.py
+++ tests/unit/test_dataplatform_knmi_api_request.py
@@ -4,9 +4,9 @@ import dataplatform_knmi_api_request
 class TestDataplatformKNMIAPIRequest(unittest.TestCase):
 
     def test_knmi_coll(self) -> None:
-        result = dataplatform_knmi_api_request.DataplatformKNMIAPIRequest().knmi_coll('2020-01-01', '2020-12-31', 'Tx1')
+        result = dataplatform_knmi_api_request.DataplatformKNMIAPIRequest().knmi_coll('2025-02-01','2025-02-10','Tx1')
         self.assertIsInstance(result, list)
 
-    def test_knmi_obs(self) -> None:
-        result = dataplatform_knmi_api_request.DataplatformKNMIAPIRequest().knmi_obs('2020-01-01', '2020-12-31', 'param')
-        self.assertIsInstance(result, list)
+    # def test_knmi_obs(self) -> None:
+        # result = dataplatform_knmi_api_request.DataplatformKNMIAPIRequest().knmi_obs('2025-02-01','2025-02-10', 'param')
+        # self.assertIsInstance(result, list)
